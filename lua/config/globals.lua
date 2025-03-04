7347a7bb92b91fe0a024bb68f0f87c6069c31278
pdj107 <cxz541@gmail.com>
2025-03-04 15:40:30 +0900

    first commit

---
+++ lua/config/globals.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " " -- global leader
+vim.g.maplocalleader = " " -- local leader
+vim.api.nvim_set_option("clipboard", "unnamedplus")
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,34 @@
+local keyMapper = require('utils.keyMapper').mapKey
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require('mason').setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = { "lua_ls", "ts_ls", "gopls" , "pylyzer", "rust_analyzer", "clangd" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.pylyzer.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.clangd.setup({})
+
+      keyMapper('K', vim.lsp.buf.hover)
+      keyMapper('gd', vim.lsp.buf.definition)
+      keyMapper('<leader>ca', vim.lsp.buf.code_action)
+
+    end
+  }
+}
